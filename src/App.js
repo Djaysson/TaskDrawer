d9decc0fa4ffdb43cb0e088fe6e4fd29d9109933
Djaysson <djayson.dev@gmail.com>
2023-01-11 23:05:58 -0300

    Add:Create Task Panel Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Board } from "./components/Board/index";
 import { Header } from "./components/Header";
 import GlobalStyle from "./styles/global";
+
 function App() {
   return (
     <>
       <Header />
+      <Board />
       <GlobalStyle />
     </>
   );
---
+++ src/components/Board/index.jsx
@@ -0,0 +1,12 @@
+import { List } from "../List";
+import { Container } from "./styles";
+
+export function Board() {
+  return (
+    <Container>
+      <List />
+      <List />
+      <List />
+    </Container>
+  );
+}
